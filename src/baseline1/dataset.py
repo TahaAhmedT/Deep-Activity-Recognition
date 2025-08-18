a254a341a0a3e3a9726018d0a68403b9f0050557
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-08-19 01:41:51 +0300

    Refactor B1Dataset class to unify target video parameter naming and enhance class methods for image and class retrieval

--- src/baseline1/dataset.py
+++ src/baseline1/dataset.py
@@ -1,11 +1,12 @@
 from torch.utils.data import Dataset
 import os
 from PIL import Image
+from src.Preprocessing.volleyball_annot_loader import load_video_annot
 
 class B1Dataset(Dataset):
-    def __init__(self, videos_root: str, target_dirs: list[int], transform=None):
+    def __init__(self, videos_root: str, target_videos: list[int], transform=None):
         self.videos_root = videos_root
-        self.target_dirs = target_dirs
+        self.target_videos = target_videos
         self.transform = transform
 
     def get_images(self):
@@ -15,7 +16,7 @@ class B1Dataset(Dataset):
 
         # Iterate on each video, in each video: iterate on each clip, in each clip: iterate on each image untill find the image that has the same id like the clip
         for idx, video_dir in enumerate(videos_dirs):
-            if idx in self.target_dirs:
+            if idx in self.target_videos:
                 video_dir_path = os.path.join(self.videos_root, video_dir)
 
                 if not os.path.isdir(video_dir_path):
@@ -36,6 +37,27 @@ class B1Dataset(Dataset):
                             images.append(Image.open(img_path))
                             break  # Stop after finding the first matching image (there is only one per clip)
         return images
+    
+    def get_classes(self):
+        classes = []
+        videos_dirs = os.listdir(self.videos_root)
+        videos_dirs.sort()
+
+        for idx, video_dir in enumerate(videos_dirs):
+            if idx in self.target_videos:
+                video_dir_path = os.path.join(self.videos_root, video_dir)
+                
+                video_annot = os.path.join(video_dir_path, 'annotations.txt')
+                clip_category_dict = load_video_annot(video_annot)
+
+                # clips_dir = os.listdir(video_dir_path)
+                # clips_dir.sort()
+
+                for _, category in clip_category_dict.items():
+                    classes.append(category)
+
+        return classes
+
 
     def __len__(self):
         pass
