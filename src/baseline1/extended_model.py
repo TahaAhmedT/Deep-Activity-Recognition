62a45ec6627cab35ca90b2ba2102b9531069e402
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-09-01 03:30:31 +0300

    Refactor ExtendedModel to simplify fully connected layers and ensure correct output shape

--- src/baseline1/extended_model.py
+++ src/baseline1/extended_model.py
@@ -5,16 +5,12 @@ class ExtendedModel(nn.Module):
     def __init__(self, backbone):
         super(ExtendedModel, self).__init__()
         self.backbone = backbone
-        self.fc_layers = nn.Sequential(
-            nn.Linear(512 * 28 * 28, 265),
-            nn.ReLU(),
-            nn.Linear(265, 128),
-            nn.ReLU(),
-            nn.Linear(128, 8)  # we have 8 classes in our dataset
+        self.fc_layer = nn.Sequential(
+            nn.Linear(2048, 8)  # we have 8 classes in our dataset
         )
 
     def forward(self, x):
         x = self.backbone(x)
         x = x.view(x.size(0), -1)
-        x = self.fc_layers(x)
+        x = self.fc_layer(x)
         return x
