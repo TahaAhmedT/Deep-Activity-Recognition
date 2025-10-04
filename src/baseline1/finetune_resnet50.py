48517bedac0b50dce86ce2d5606f6cf1c6c7aad2
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-10-05 01:59:32 +0300

    Refactor logging in finetune_resnet50.py to use logger instead of print statements

--- src/baseline1/finetune_resnet50.py
+++ src/baseline1/finetune_resnet50.py
@@ -3,7 +3,7 @@ from src.baseline1.extended_model import ExtendedModel
 from src.utils.train_test.train_step import train_step
 from src.utils.train_test.test_step import test_step
 from src.utils.config_utils.load_config import load_config
-from src.utils.stream_utils.stream_utils import log_stream
+from src.utils.logging_utils.logging_utils import setup_logger
 
 import torch
 import torch.nn as nn
@@ -11,6 +11,13 @@ import torch.optim as optim
 from torchvision import transforms
 from torchvision.models import resnet50, ResNet50_Weights
 from torch.utils.data import DataLoader
+
+logger = setup_logger(
+            log_file=__file__,
+            log_dir="logs\baselines_logs\baseline1_logs",
+            log_to_console=True,
+            use_tqdm=True,
+        )
 
 def get_data_loaders(config, verbose=False):
     """Creates train and test data loaders.
@@ -22,8 +29,7 @@ def get_data_loaders(config, verbose=False):
     Returns:
         tuple: (trainloader, testloader)
     """
-    if verbose:
-        print("[INFO] Creating data loaders...")
+    logger.info("[INFO] Creating data loaders...")
     transform = transforms.Compose([
         transforms.Resize((256, 256)),
         transforms.CenterCrop((224, 224)),
@@ -43,8 +49,7 @@ def get_data_loaders(config, verbose=False):
     )
     trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-    if verbose:
-        print("[INFO] Data loaders created.")
+    logger.info("[INFO] Data loaders created.")
     return trainloader, testloader
 
 def get_model(verbose=False):
@@ -56,14 +61,12 @@ def get_model(verbose=False):
     Returns:
         nn.Module: Modified ResNet50 model wrapped in ExtendedModel.
     """
-    if verbose:
-        print("[INFO] Initializing ResNet50 model...")
+    logger.info("[INFO] Initializing ResNet50 model...")
     original_model = resnet50(weights=ResNet50_Weights.DEFAULT, progress=verbose)
     layers = list(original_model.children())[:-1]
     truncated_model = nn.Sequential(*layers)
     model = ExtendedModel(truncated_model)
-    if verbose:
-        print("[INFO] Model initialized and truncated.")
+    logger.info("[INFO] Model initialized and truncated.")
     return model
 
 def get_optimizer(model, config, verbose=False):
@@ -77,12 +80,11 @@ def get_optimizer(model, config, verbose=False):
     Returns:
         torch.optim.Optimizer: AdamW optimizer.
     """
-    if verbose:
-        print("[INFO] Creating optimizer...")
+    logger.info("[INFO] Creating optimizer...")
     lr = config["TRAINING_PARAMS"]["lr"]
     optimizer = optim.AdamW(model.parameters(), lr=lr)
-    if verbose:
-        print(f"[INFO] Optimizer created with learning rate {lr}.")
+    logger.info("[INFO] Optimizer created: AdamW Optimizer.")
+    logger.info(f"[INFO] Learning Rate: {lr}.")
     return optimizer
 
 def main(verbose=True):
@@ -92,21 +94,19 @@ def main(verbose=True):
         verbose (bool): If True, prints info logs.
     """
     CONFIG = load_config()
-    log_stream(log_file="finetune_resnet50_logs", prog="baselines_logs/baseline1_logs", verbose=True)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    if verbose:
-        print(f"[INFO] Using device: {device}")
+    logger.info(f"[INFO] Using device: {device}")
     trainloader, testloader = get_data_loaders(CONFIG, verbose=verbose)
     model = get_model(verbose=verbose)
     criterion = nn.CrossEntropyLoss()
     optimizer = get_optimizer(model, CONFIG, verbose=verbose)
     num_epochs = CONFIG["TRAINING_PARAMS"]["num_epochs"]
+    logger.info(f"[INFO] Starting Training and Testing with number of epochs = {num_epochs}")
 
     for epoch in range(num_epochs):
-        print(f"Epoch {epoch+1}\n-----------------------")
+        logger.info(f"Epoch {epoch+1}\n-----------------------")
         # Training step
-        if verbose:
-            print("[INFO] Starting training step...")
+        logger.info("[INFO] Starting training step...")
         train_step(
             data_loader=trainloader,
             model=model,
@@ -114,9 +114,8 @@ def main(verbose=True):
             optimizer=optimizer,
             device=device
         )
-        if verbose:
-            print("[INFO] Training step completed.")
-            print("[INFO] Starting testing step...")
+        logger.info("[INFO] Training step completed.")
+        logger.info("[INFO] Starting testing step...")
         # Testing step
         test_step(
             data_loader=testloader,
@@ -124,8 +123,7 @@ def main(verbose=True):
             loss_fn=criterion,
             device=device
         )
-        if verbose:
-            print("[INFO] Testing step completed.")
+        logger.info("[INFO] Testing step completed.")
 
 if __name__ == "__main__":
     main()
