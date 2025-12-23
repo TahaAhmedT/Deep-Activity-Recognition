391bf889cd3cd2b3b3072199afe6a7b602aa71e7
TahaAhmedT <tahaahmedghoba4y@gmail.com>
2025-12-23 21:10:37 +0200

    Add tester scripts for B1 to B8 models

---
+++ src/testers/b1_tester.py
@@ -0,0 +1,28 @@
+from utils.config_utils import load_config
+from utils.test_utils import test
+
+def main():
+    """
+    Main entry point to test B1 Model.
+    """
+
+    CONFIG = load_config()
+    
+    test(log_dir=CONFIG["baseline1_logs"],
+        batch_size=CONFIG["TRAINING_PARAMS"]["batch_size"],
+        videos_root=CONFIG["DATA_PATHS"]["videos_root"],
+        annot_root=CONFIG["DATA_PATHS"]["annot_root"],
+        train_ids=CONFIG["TARGET_VIDEOS"]["train_ids"],
+        test_ids=CONFIG["TARGET_VIDEOS"]["val_ids"],
+        features=False,
+        model_name="b1",
+        checkpoint="models/b1/best_model.pth",
+        num_classes=CONFIG["NUM_LABELS"],
+        actions_dict=CONFIG["CATEGORIES_DICT"],
+        save_path="assets/test_assets",
+        figname="B1_Confusion_Matrix",
+        image_level=True,
+        crop=False,
+        verbose=CONFIG["verbose"]
+    )
+    
---
+++ src/testers/b4_tester.py
@@ -0,0 +1,30 @@
+from utils.config_utils import load_config
+from utils.test_utils import test
+
+def main():
+    """
+    Main entry point to test B4 Model.
+    """
+
+    CONFIG = load_config()
+    
+    test(log_dir=CONFIG["baseline4_logs"],
+        batch_size=CONFIG["TRAINING_PARAMS"]["batch_size"],
+        videos_root=CONFIG["DATA_PATHS"]["videos_root"],
+        annot_root=CONFIG["DATA_PATHS"]["annot_root"],
+        train_ids=CONFIG["TARGET_VIDEOS"]["train_ids"],
+        test_ids=CONFIG["TARGET_VIDEOS"]["val_ids"],
+        features=True,
+        model_name="b4",
+        checkpoint="models/b4/best_model.pth",
+        num_classes=CONFIG["NUM_LABELS"],
+        actions_dict=CONFIG["CATEGORIES_DICT"],
+        save_path="assets/test_assets",
+        figname="B4_Confusion_Matrix",
+        output_file=CONFIG["DATA_PATHS"]["frame_features_root"],
+        input_size=CONFIG["EXTRACTED_FEATURES_SIZE"],
+        hidden_size1=CONFIG["HIDDEN_SIZE1"],
+        num_layers=CONFIG["NUM_LAYERS"],
+        sequence=True,
+        verbose=CONFIG["verbose"]
+    )
